7a5f0dc88d973a5f9661086401630c20f94554bd
ISHIDA Naoto <naoto@isnot.jp>
2020-07-16 20:51:27 +0900

    support hexo 5.0 injector

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 /**
  * Created by lzane on 8/27/17.
  */
-var renderer = require('./src/renderer');
+const renderer = require('./src/renderer');
+
+function addCssTag() {
+  const config = this.config.lightgallery;
+  if (!config) {
+    return '';
+  }
+  return `<link rel="stylesheet" type="text/css" href="${config.css}" />`;
+}
+
+function addJsTag() {
+  const config = this.config.lightgallery;
+  if (!config) {
+    return '';
+  }
+  const plugins = Object.keys(config.plugins);
+  let jsTag = `<script src="${config.js}"></script>`;
+  for (const plugin of plugins) {
+    jsTag += `<script src="${config.plugins[plugin]}"></script>`;
+  }
+  return `${jsTag}
+    <script>if (typeof lightGallery !== 'undefined') {
+      const options = { selector: '.gallery-item' };
+      const gallery = Array.from(document.getElementsByClassName('article-gallery'));
+      for (const target of gallery) {
+        lightGallery(target, options);
+      }
+    }</script>`;
+}
 
 hexo.config.lightgallery = Object.assign({
-    js: 'https://cdn.jsdelivr.net/lightgallery.js/1.0.1/js/lightgallery.min.js',
-    css: 'https://cdn.jsdelivr.net/lightgallery.js/1.0.1/css/lightgallery.min.css',
-    plugins: {},
+  js: 'https://cdn.jsdelivr.net/lightgallery.js/1.0.1/js/lightgallery.min.js',
+  css: 'https://cdn.jsdelivr.net/lightgallery.js/1.0.1/css/lightgallery.min.css',
+  plugins: {},
 }, hexo.config.lightgallery);
 
 hexo.extend.filter.register('after_post_render',renderer.render,9);
+hexo.extend.injector.register('head_end', addCssTag, 'default');
+hexo.extend.injector.register('body_end', addJsTag, 'default');
--- src/renderer.js
+++ src/renderer.js
@@ -1,77 +1,41 @@
 /**
  * Created by lzane on 8/20/17.
  */
-var imgRgr = /<img [^>]+>(?!\s*<\/a>)/g;
+const imgRgr = /<img [^>]+>(?!\s*<\/a>)/g;
 
 function ignore(data) {
-  var source = data.source;
-  var ext = source.substring(source.lastIndexOf('.')).toLowerCase();
+  const source = data.source;
+  const ext = source.substring(source.lastIndexOf('.')).toLowerCase();
   return ['.js', '.css', '.html', '.htm'].indexOf(ext) > -1;
 }
 
-function addTag(data){
-    var config = this.config.lightgallery;
-    if(!config){
-        return;
+function wrapImage(data) {
+  data.content = `<div class="article-gallery">${data.content}</div>`;
+  data.content = data.content.replace(imgRgr, function replace(match) {
+    let isSrcMatched = false;
+    let res = '<a ';
+    const hrefResult = /src\s*=\s*"([^"]+?)"(?!.+?nolink)/g.exec(match);
+    if (Array.isArray(hrefResult)) {
+      res += `href="${hrefResult[1]}" `;
+      isSrcMatched = true;
     }
-
-    // add js
-    data.content = '<div class="article-gallery">'+data.content+'</div>';
-    data.content+='<script src="'+config.js+'"></script>';
-
-    // add css
-    var css = '<link rel="stylesheet" type="text/css" href="'+config.css+'" />';
-    data.content = css + data.content;
-
-    // add plugins
-    var plugins = Object.keys(config.plugins);
-    for (var plugin of plugins){
-        var jsTag = '<script src="'+config.plugins[plugin]+'"></script>';
-        data.content += jsTag;
+    const titleResult = /alt\s*=\s*"(.+?)"/g.exec(match);
+    if (Array.isArray(titleResult)) {
+      res += `title="${titleResult[1]}" `;
     }
+    res += `class="gallery-item">${match}</a>`;
+    if (isSrcMatched) {
+      return res;
+    }
+    return match;
+  });
 }
 
-function addRunnableTag(data){
-    data.content+='<script>'+
-        `if (typeof lightGallery !== 'undefined') {
-        var options = {
-            selector: '.gallery-item'
-        };
-        lightGallery(document.getElementsByClassName('article-gallery')[0], options);
-        }`
-        +'</script>';
-}
-
-function wrapImage(data){
-    data.content = data.content.replace(imgRgr, function replace(match){
-        var isSrcMatched = false;
-        var res = '<a ';
-        var hrefResult = /src\s*=\s*"([^"]+?)"(?!.+?nolink)/g.exec(match);
-        if (hrefResult){
-            var href = hrefResult[1];
-            res+='href="'+href+'" ';
-            isSrcMatched = true;
-        }
-        var titleResult = /alt\s*=\s*"(.+?)"/g.exec(match);
-        if (titleResult){
-            var title = titleResult[1];
-            res+='title="'+title+'" ';
-        }
-        res+='class="gallery-item">'+match+'</a>';
-        if (isSrcMatched) {
-            return res;
-        }
-        return match;
-    });
-}
-
-function render(data){
-    if (ignore(data)){
-        return;
-    }
-    wrapImage.call(this,data);
-    addTag.call(this,data);
-    addRunnableTag.call(this,data);
+function render(data) {
+  if (ignore(data)) {
+    return;
+  }
+  wrapImage.call(this,data);
 };
 
 exports.render = render;
